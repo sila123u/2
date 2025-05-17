8d4a6fbc121fdba061c85964258b797aa501e2bc
sila123u <silaguler6u@gmail.com>
2025-05-18 01:37:24 +0300

    Add files via upload

---
+++ odev7.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("p2.jpg")
+olcek_yuzde = 40
+genislik = int(img.shape[1] * olcek_yuzde / 100)
+yukseklik = int(img.shape[0] * olcek_yuzde / 100)
+boyut = (genislik, yukseklik)
+resized = cv2.resize(img, boyut, interpolation=cv2.INTER_AREA)
+
+gri = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+gri = cv2.GaussianBlur(gri, (9, 9), 2)
+
+circles = cv2.HoughCircles(
+    gri,
+    cv2.HOUGH_GRADIENT,
+    dp=1.2,
+    minDist=90,
+    param1=100,
+    param2=60,
+    minRadius=65,
+    maxRadius=115
+)
+
+sayi = 0
+if circles is not None:
+    circles = np.uint16(np.around(circles))
+    for (x, y, r) in circles[0, :]:
+        cv2.circle(resized, (x, y), r, (0, 255, 0), 3)
+        sayi += 1
+
+cv2.putText(resized, f"Para Sayisi: {sayi}", (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 0), 3)
+
+cv2.imshow("Sonuc", resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
